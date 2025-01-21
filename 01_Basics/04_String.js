832c5cd755c708ac195f288699b994ab2ef548cd
Madhurajayawant <45929663+Madhurajayawant@users.noreply.github.com>
2025-01-21 13:18:57 +0000

    added string

---
+++ 01_Basics/04_String.js
@@ -0,0 +1,18 @@
+const name = "Madhura"
+const age = 35
+
+//Concatanation 
+console.log(name + age + "Engineer"); //Not Recommended
+
+//Interpolation
+console.log(`Hello my name is ${name} and my age is ${age}`);
+
+/*
+Way of declaring string
+
+1. const name = "Madhura"
+2. const name = new String('Madhura') - this declaration gives lots of methods
+Examples are below this comments
+*/
+
+const name1 = new String('Madhura')
