57acffe0472079e5cfb98f280a48e5b306fd2765
Madhurajayawant <45929663+Madhurajayawant@users.noreply.github.com>
2025-01-21 11:29:59 +0000

    dataType

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,16 @@
+const accountId = 123456
+let accountName = "Madhura"
+var accountPassword = "12345"
+
+accountCity = "Pune"
+
+// accountId = 2  // This is not allowed as it is const
+console.log(accountId);
+console.table([accountId , accountName, accountPassword , accountCity])
+
+/*
+Const - is const throughout the files
+Let - keyword is applicable in between the defined scope
+Var - keyword is not user now a days, no scoping is applicable with var keyword
+
+*/
---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,15 @@
+// datatypes
+
+/* Premitive data type ---
+Number, string boolean, big Int = greater than 2 to the power 53
+
+Null : empty value
+undefine : no value is assign
+Symbol : 
+
+Non Premitive
+Object
+Typeof
+
+
+*/
