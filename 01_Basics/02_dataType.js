935331b180ddd807502ad809079f9c16aa6d5777
Madhurajayawant <45929663+Madhurajayawant@users.noreply.github.com>
2025-01-21 12:31:12 +0000

    added operation file

--- 01_Basics/02_dataType.js
+++ 01_Basics/02_dataType.js
@@ -1,15 +1,44 @@
 // datatypes
 
 /* Premitive data type ---
-Number, string boolean, big Int = greater than 2 to the power 53
+Number, string, boolean, big Int = greater than 2 to the power 53
 
 Null : empty value
 undefine : no value is assign
-Symbol : 
+Symbol : to make any value special, to create unique value
 
-Non Premitive
-Object
-Typeof
+How to declare symbol datatype
+const id = Symbol('123')
+const newId = Symbol('123)
+The id and newId both are different
+====Non Premitive data types====
+
+1 ) Object - define in key value pair
+{
+name = "Madhura",
+age = "35"
+}
+
+we can store the object in variable
+
+let MyData = {
+name = "Madhura",
+age = "35"
+}
+
+2) Arrays - ["Madhura", "Vrushali","Dimple"]
+
+3) Functions -
+
+syntax
+
+function(){
+}
+
+we can store the object in variable
+let myFunction = function(){
+}
+
 
 
 */
